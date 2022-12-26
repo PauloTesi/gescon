bc7129963f30c5e2f0ec92a81b929dc2816b8e32
Paulo Melo <paulo.tesi@gmail.com>
2022-12-26 14:51:24 -0300

    Pasta com inicial maiuscula

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
 FROM node:16
 
 # Create app directory
-WORKDIR /app
+WORKDIR /aplicacao
 
 # Install app dependencies
 # A wildcard is used to ensure both package.json AND package-lock.json are copied
 # where available (npm@5+)
-COPY package*.json /app
+COPY package*.json /aplicacao
 
 RUN npm install --silent
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
-import { RodapeComponent } from './componentes/rodape/rodape.component';
-import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
-import { FormsModule } from '@angular/forms';
-import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
-import { PensamentoComponent } from './componentes/pensamentos/pensamento/pensamento.component';
-import { HttpClientModule } from '@angular/common/http';
-import { ExcluirPensamentoComponent } from './componentes/pensamentos/excluir-pensamento/excluir-pensamento.component';
-import { EditarPensamentoComponent } from './componentes/pensamentos/editar-pensamento/editar-pensamento.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component";
+import { RodapeComponent } from "./componentes/rodape/rodape.component";
+import { CriarPensamentoComponent } from "./componentes/pensamentos/criar-pensamento/criar-pensamento.component";
+import { FormsModule } from "@angular/forms";
+import { ListarPensamentoComponent } from "./componentes/pensamentos/listar-pensamento/listar-pensamento.component";
+
+import { HttpClientModule } from "@angular/common/http";
+import { ExcluirPensamentoComponent } from "./componentes/pensamentos/excluir-pensamento/excluir-pensamento.component";
+import { EditarPensamentoComponent } from "./componentes/pensamentos/editar-pensamento/editar-pensamento.component";
+import { PensamentoComponent } from "./componentes/Pensamentos/pensamento/pensamento.component";
 
 @NgModule({
   declarations: [
@@ -22,15 +23,10 @@ import { EditarPensamentoComponent } from './componentes/pensamentos/editar-pens
     ListarPensamentoComponent,
     PensamentoComponent,
     ExcluirPensamentoComponent,
-    EditarPensamentoComponent
+    EditarPensamentoComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
